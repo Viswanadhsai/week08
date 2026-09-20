0387e6c4d06315d52fe8082e6447ac7eda50af83
Viswanadhsai <viswanadhsai35@gmail.com>
2026-09-20 12:30:12 +1000

    Provision ACR and fix CI build pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,14 @@ data "azurerm_resource_group" "main" {
   name = "deakinuni"
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "viswa722acr102ds226051707"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
 resource "azurerm_storage_account" "task102d" {
   name                     = "viswa102ds226051707"
   resource_group_name      = data.azurerm_resource_group.main.name
@@ -25,6 +33,14 @@ resource "azurerm_storage_account" "task102d" {
 
 output "resource_group_name" {
   value = data.azurerm_resource_group.main.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
 
 output "storage_account_name" {
