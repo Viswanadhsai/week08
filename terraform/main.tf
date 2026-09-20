f9240809a178631bc73b1f13baa8f9095a8efd3d
Viswanadhsai <viswanadhsai35@gmail.com>
2026-09-20 12:18:24 +1000

    Update Terraform for 10.2D infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,37 +11,22 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "week08" {
-  name = "VISWA-RG08"
+data "azurerm_resource_group" "main" {
+  name = "deakinuni"
 }
 
-data "azurerm_container_registry" "acr" {
-  name                = "viswa722acr08"
-  resource_group_name = data.azurerm_resource_group.week08.name
+resource "azurerm_storage_account" "task102d" {
+  name                     = "viswa102ds226051707"
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = data.azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = "viswa722aks08"
-  resource_group_name = data.azurerm_resource_group.week08.name
+output "resource_group_name" {
+  value = data.azurerm_resource_group.main.name
 }
 
-data "azurerm_storage_account" "storage" {
-  name                = "viswa722storage08"
-  resource_group_name = data.azurerm_resource_group.week08.name
-}
-
-output "resource_group" {
-  value = data.azurerm_resource_group.week08.name
-}
-
-output "acr_name" {
-  value = data.azurerm_container_registry.acr.name
-}
-
-output "aks_name" {
-  value = data.azurerm_kubernetes_cluster.aks.name
-}
-
-output "storage_name" {
-  value = data.azurerm_storage_account.storage.name
+output "storage_account_name" {
+  value = azurerm_storage_account.task102d.name
 }
